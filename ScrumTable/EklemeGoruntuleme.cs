e3a77e7c11418c2558ade37b4af1c8808c097d3f
Mustafa_CEVIK <cevikksoft@gmail.com>
2018-05-23 15:12:26 +0300

    Yeni projeye geçildi, isimlendirmeler ve veritabanı bağlantısı tamamlandı.

---
+++ ScrumTable/EklemeGoruntuleme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ScrumTable
+{
+    public partial class EklemeGoruntuleme : Form
+    {
+        public EklemeGoruntuleme()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection veriBaglantisi = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\bin\\Debug\\Veriler.mdb");
+
+        private void VerilerGoruntule()
+        {
+            veriBaglantisi.Open();
+            OleDbCommand veriKomutu = new OleDbCommand();
+            veriKomutu.Connection = veriBaglantisi;
+            veriKomutu.CommandText = ("Select * from Verier");
+            OleDbDataReader veriOku = veriKomutu.ExecuteReader();
+            while (veriOku.Read())
+            {
+
+            }
+        }
+
+        private void btn_Ekle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    //Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\bin\Debug\Veriler.mdb
+}
---
+++ ScrumTable/ScrumTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrumTable
+{
+    public partial class frm_ScrumTable : Form
+    {
+        public frm_ScrumTable()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_AddStory_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
